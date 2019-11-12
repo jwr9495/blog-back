4722acb9f6867c4f5ae0635d5e5b3d3404c278d4
jwr9495 <skdlwn156@naver.com>
2019-11-12 15:26:49 +0900

    login complete

--- routers/user.js
+++ routers/user.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const { User, validateUser } = require("../models/user");
+const { jwtSecret } = require("../common/jwt_config");
 
 router.post("/join", async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -17,6 +18,36 @@ router.post("/join", async (req, res, next) => {
   const saveResult = await user.save();
   res.json({ result: true });
   next();
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(email);
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    res.json({ result: false });
+    next();
+    return;
+  }
+  const result = await bcrypt.compare(password, user.password);
+  if (result) {
+    //create token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin
+      },
+      jwtSecret,
+      { expiresIn: "1m" }
+    );
+    res.json({ result: true, token, admin: user.admin });
+    next();
+  } else {
+    res.json({ result: false });
+    next();
+  }
 });
 
 module.exports = router;
